75eced8342ffe2a2586b6c9861765a02c02a2e00
Chlamydomonos <2457242458@qq.com>
2020-11-24 11:37:56 +0800

    Made some mild changes

--- src/command/commands/load.cpp
+++ src/command/commands/load.cpp
@@ -11,15 +11,15 @@ erisc::Load::~Load()
 
 void erisc::Load::run(VM* vm)
 {
-	if (!(params[0].type == ParamType::REGISTER && params[1].type == ParamType::REGISTER))
-		throw Exception("Param type error");
 	try
 	{
+		if (!(params[0].type == ParamType::REGISTER && params[1].type == ParamType::REGISTER))
+			throw Exception("Param type error");
 		int value = params[1].getValue(vm);
 		vm->getRegister(params[0].value)->write(value);
 	}
 	catch (Exception& e)
 	{
-		throw;
+		throw Exception("Failed to run command \"load\"", e);
 	}
 }
