d592295ab7fc55edbefb6ac85b58e9785c2deaba
phantom-1123 <zhouzhaoyi123@163.com>
2020-11-29 12:18:54 +0800

    Mov finished

---
+++ src/command/commands/mov.cpp
@@ -0,0 +1,27 @@
+#include "mov.h"
+#include "../../vm/vm.h"
+
+erisc::Mov::Mov(char* paramString):erisc::Command(2,paramString)
+{
+}
+
+erisc::Mov::~Mov()
+{
+}
+
+void erisc::Mov::run(VM* vm)
+{
+	try
+	{
+		if (!(params[0].type == ParamType::REGISTER && (params[1].type == ParamType::NUMBER || params[1].type == ParamType::REGISTER)))
+		{
+			throw Exception("Param type error");
+		}
+		int value = params[1].getValue(vm);
+		vm->getRegister(params[0].value)->write(value);
+	}
+	catch (Exception& e)
+	{
+		throw Exception("Failed to run command \"mov\"", e);
+	}
+}
