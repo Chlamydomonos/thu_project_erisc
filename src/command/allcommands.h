fa58730d0ba20b5d39df72f7bd594322315ebdd6
phantom-1123 <zhouzhaoyi123@163.com>
2020-12-14 17:15:06 +0800

    allcommands almost finished

--- src/command/allcommands.h
+++ src/command/allcommands.h
@@ -1,6 +1,26 @@
 #ifndef ALLCOMMANDS_H
 #define ALLCOMMANDS_H
 #include "command.h"
-
+#include "commands/add.h"
+#include "commands/and.h"
+#include "commands/beq.h"
+#include "commands/bge.h"
+#include "commands/blt.h"
+#include "commands/bne.h"
+#include "commands/cal.h"
+#include "commands/div.h"
+#include "commands/end.h"
+#include "commands/jal.h"
+#include "commands/load.h"
+#include "commands/mov.h"
+#include "commands/mul.h"
+#include "commands/or.h"
+#include "commands/pop.h"
+#include "commands/push.h"
+#include "commands/rem.h"
+#include "commands/ret.h"
+#include "commands/store.h"
+#include "commands/sub.h"
+#include "lineid.h"
 
 #endif // !ALLCOMMANDS_H
