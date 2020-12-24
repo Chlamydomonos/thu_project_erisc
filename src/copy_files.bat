7ca4ef32eec9fd808f9ececf337df1086b70b6df
Chlamydomonos <2457242458@qq.com>
2020-12-25 00:15:10 +0800

    Tried to reformat

--- src/copy_files.bat
+++ src/copy_files.bat
@@ -1,4 +1,5 @@
-md %2images
-for %%i in (%1src\image\*.bmp) do (copy "%%i" %2images)
-for %%i in (%1src\image\*.imgcfg) do (copy "%%i" %2images)
-for %%i in (%1src\*.vmcfg) do (copy "%%i" %2)
+cd %1
+
+for %%i in (image/*.bmp) do copy "%%i" %2images
+copy image/image.imgcfg %2images
+copy vm.vmcfg %2
