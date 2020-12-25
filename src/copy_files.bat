7bea69f19c68ae248249a2cd9f1aac6383ee8318
Chlamydomonos <2457242458@qq.com>
2020-12-25 15:14:44 +0800

    Fixed copy_files

--- src/copy_files.bat
+++ src/copy_files.bat
@@ -1,5 +1,6 @@
 cd %1
-
-for %%i in (image/*.bmp) do copy "%%i" %2images
-copy image/image.imgcfg %2images
+cd image
+for %%i in (*.bmp) do copy "%%i" %2images
+copy image.imgcfg %2images
+cd %1
 copy vm.vmcfg %2
